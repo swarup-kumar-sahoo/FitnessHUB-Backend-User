0bd1271c45de98dfe5cfc85b2d680bfb12f63fe4
Swarup Kumar Sahoo <105979920+swarup-kumar-sahoo@users.noreply.github.com>
2025-09-13 20:09:20 +0530

    Updated Port

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
